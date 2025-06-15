87822c0f02fcf82087867c3656c1add915ff0d04
heyharsh50 <harshraj88253@gmail.com>
2025-06-16 00:24:16 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-   base: "/HarsH-Pfolio",
+   base: "/HarsH-Pfolio/",
   plugins: [
     react(),
     mode === 'development' &&
