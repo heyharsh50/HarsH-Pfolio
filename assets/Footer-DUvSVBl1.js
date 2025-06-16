1e49cf09ac5f89fc0b31794b4b72a36aa97f1eaf
heyharsh50 <harshraj88253@gmail.com>
2025-06-16 13:06:37 +0530

    Updates

--- assets/Footer-DUvSVBl1.js
+++ assets/Footer-DUvSVBl1.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-DPOUzvIk.js";/**
+import{c as t,j as e}from"./index-3CAAyO9U.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
