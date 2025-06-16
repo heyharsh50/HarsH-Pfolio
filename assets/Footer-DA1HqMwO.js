2be64f2f2eade9eb12913fae6b283b1d89e7027a
heyharsh50 <harshraj88253@gmail.com>
2025-06-16 13:19:15 +0530

    Updates

--- assets/Footer-DA1HqMwO.js
+++ assets/Footer-DA1HqMwO.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-3CAAyO9U.js";/**
+import{c as t,j as e}from"./index-NXyJSKBE.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Resume-B-XejcQ8.js
+++ assets/Resume-B-XejcQ8.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,D as c,C as x,S as h}from"./index-3CAAyO9U.js";/**
+import{c as t,j as e,D as c,C as x,S as h}from"./index-NXyJSKBE.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
